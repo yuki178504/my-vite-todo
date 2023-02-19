fb2e64ba64b35e3a70bad63832a0ff46619001c8
yuki178504 <aerosmit1989x@gmail.com>
2023-02-19 22:07:20 +0900

    [add]APIデータを取得してくる方法を実装

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -5,6 +5,14 @@ const route = useRoute();
 const id = ref(route.params.id);
 console.log('watch外:', id);
 
+const posts = ref([]);
+const fetchData = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  posts.value = await response.json();
+};
+fetchData();
+console.log(posts);
+
 watch(route, () => {
   id.value = route.params.id;
   console.log('watch内:', id.value);
@@ -12,6 +20,28 @@ watch(route, () => {
 </script>
 
 <template>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      {{ post.id }}
+      <router-link :to="`blog/${post.id}`">{{ post.title }}</router-link>
+    </li>
+  </ul>
   <p>blogPage</p>
   <p>blog idは{{ id }}</p>
 </template>
+
+<style scoped>
+ul {
+  margin-top: 12px;
+}
+
+li {
+  margin-bottom: 8px;
+  border: 1px solid #ccc;
+  padding: 8px;
+}
+
+li:hover {
+  background-color: #eee;
+}
+</style>
